9d20aed694ffe9d2f1dae434a69722026f35401a
leejinsil0408 <hby04011@naver.com>
2022-07-28 14:48:24 +0900

    SpringBootExample

---
+++ src/main/java/com/example/dogproject/DogProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.dogproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogProjectApplication {
+    //시작점
+    public static void main(String[] args) { SpringApplication.run(DogProjectApplication.class, args); }
+
+
+}
---
+++ src/test/java/com/example/dogproject/DogProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dogproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
